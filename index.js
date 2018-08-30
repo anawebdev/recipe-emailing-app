9cdffce1c2548493c0cb3783f6dad1f73dfd365c
anawebdev <ana.toma92@gmail.com>
2018-08-30 08:06:12 +0300

    msg

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); //generates a new app
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({hello: 'there'});
 });
 
 //app.get creates a new route handler. waiting for an http request with a specific handler
