cae9898fbadc8f13ae4a4592c7edd8e7a51730bf
anawebdev <ana.toma92@gmail.com>
2018-08-30 07:26:45 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express(); //generates a new app
+
+app.get('/', (req, res) => {
+    res.send({hi: 'there'});
+});
+
+//app.get creates a new route handler. waiting for an http request with a specific handler
+// '/'  it watches for requests that are accesing a particular route
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
